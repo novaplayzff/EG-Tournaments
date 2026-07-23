8dc89298396ecfbc02099437e8d361fe7e987278
novaplayzff <novaplayzff@gmail.com>
2026-07-23 15:38:45 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,38 @@
+// Firebase App import
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+
+// Firebase Authentication import
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+// Firebase Console se mila hua code yahan ayega
+const firebaseConfig = {
+
+apiKey: "YOUR_API_KEY",
+
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+projectId: "YOUR_PROJECT_ID",
+
+storageBucket: "YOUR_BUCKET",
+
+messagingSenderId: "YOUR_ID",
+
+appId: "YOUR_APP_ID"
+
+};
+
+
+// Firebase start karna
+const app = initializeApp(firebaseConfig);
+
+
+// Login system activate karna
+const auth = getAuth(app);
+
+
+// Dusri files me use karne ke liye export
+export { auth };
