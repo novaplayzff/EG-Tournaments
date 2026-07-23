7b981a01823cf828dfe54f401bcbe633504121da
novaplayzff <novaplayzff@gmail.com>
2026-07-24 01:33:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 function openPopup(name){
 
-document.getElementById("joinPopup").style.display="flex";
+    document.getElementById("joinPopup").style.display = "flex";
 
-document.getElementById("tournamentName").innerHTML=name;
+    document.getElementById("tournamentName").innerHTML = name;
 
 }
 
-
 function closePopup(){
 
-document.getElementById("joinPopup").style.display="none";
+    document.getElementById("joinPopup").style.display = "none";
+
+}
+
+function submitForm(){
+
+    let name = document.getElementById("playerName").value;
+    let uid = document.getElementById("playerUID").value;
+    let email = document.getElementById("playerEmail").value;
+    let phone = document.getElementById("playerPhone").value;
+    let payment = document.getElementById("paymentMethod").value;
+    let transaction = document.getElementById("transactionId").value;
+
+    if(name=="" || uid=="" || email=="" || phone=="" || payment=="Select Payment Method" || transaction==""){
+
+        alert("Please fill all fields.");
+        return;
+
+    }
+
+    alert("Registration Submitted Successfully!");
+
+    closePopup();
 
 }
