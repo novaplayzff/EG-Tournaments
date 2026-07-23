24e77f14d4f7baa55b8daf383177a88802147d84
novaplayzff <novaplayzff@gmail.com>
2026-07-24 02:08:59 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD6842PdiIFSU_H_jF_LXIIYI9IfqvsB80",
   authDomain: "eg-tournaments-c5bff.firebaseapp.com",
+  databaseURL: "https://eg-tournaments-c5bff-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "eg-tournaments-c5bff",
   storageBucket: "eg-tournaments-c5bff.firebasestorage.app",
   messagingSenderId: "491069840745",
   appId: "1:491069840745:web:bb9ca9027e8289ffbaf1f8"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Make available to other scripts
+window.firebaseDatabase = database;;
